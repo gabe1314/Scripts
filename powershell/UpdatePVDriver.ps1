41437de930bdf40709548c8c267c98ac82887691
Gabriel Sandoval <gabriel.sandoval1314@outlook.com>
2022-05-09 12:51:51 -0700

    v2
    Commit to Master

--- powershell/UpdatePVDriver.ps1
+++
@@ -1,3 +0,0 @@
- (New-Object Net.WebClient).DownloadFile('https://s3.amazonaws.com/ec2-windows-drivers-downloads/AWSPV/Latest/AWSPVDriver.zip','D:\Tools\AWSPVDriver.zip');(new-object -com shell.application).namespace('D:\Tools').CopyHere((new-object -com shell.application).namespace('D:\Tools\AWSPVDriver.zip').Items(),16) 
-msiexec /i D:\Tools\AWSPVDriverSetup.msi /quiet /log D:\Tools\AWSPVDriverLog.log 
-
